de4288763f8927434c97747f9ff6d514a0d96c2d
Johann Kellerman <kellerza@gmail.com>
2016-05-21 14:24:29 +0200

    setup version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='pyqwikswitch',
-      version='0.1',
+      version='0.2',
       description='Library to interface Qwikswitch USB Hub',
       url='https://github.com/kellerza/pyqwikswitch',
       author='Johann Kellerman',
